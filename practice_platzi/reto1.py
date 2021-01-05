f0bd26932aebf14702fa5f5aa930bfc476d31f9e
Andres galeano <afgaleano91@misena.edu.co>
2021-01-05 01:51:30 -0500

    ejercicios de practica python

---
+++ practice_platzi/reto1.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    lista = []
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        lista.append([name,score])
+    print(lista)
+
+    puntaje = float(0)
+    new_list =[]
+    
+    for objeto in lista:
+        print(objeto)
+        puntaje = 
+        if puntaje == objeto[1]:
+            new_list.append([objeto])
+        else:
+            puntaje = objeto[1]
+    print(new_list)
+
+
